1a00e90704d3b76b3796c230e3f6e38741bddc3e
Chavez <matthew@el-chavez.me>
2017-01-02 16:25:23 -0800

    Add build cancel subcommand
    - Cancel subcommand
    - Build number flag added

--- commands/build.go
+++ commands/build.go
@@ -12,15 +12,25 @@ import (
 
 // BuildCmd - build project and interact with builds
 var BuildCmd = cli.Command{
-	Name:   "build",
-	Usage:  "build project and interact with builds",
-	Flags:  buildFlags,
-	Action: buildProjectAction,
+	Name:        "build",
+	Usage:       "build project and interact with builds",
+	Flags:       buildFlags,
+	Action:      buildProjectAction,
+	Subcommands: buildSubcommands,
 }
 
 var buildFlags = []cli.Flag{
 	userFlag,
 	projectFlag,
+}
+
+var buildSubcommands = []cli.Command{
+	{
+		Name:   "cancel",
+		Usage:  "cancel a specific build",
+		Action: buildCancelAction,
+		Flags:  append(buildFlags, buildNumFlag),
+	},
 }
 
 func buildProjectAction(context *cli.Context) error {
@@ -36,5 +46,22 @@ func buildProjectAction(context *cli.Context) error {
 		return nil
 	}
 	failed := errors.New("Failed to build")
+	return failed
+}
+
+func buildCancelAction(context *cli.Context) error {
+	token := context.GlobalString("token")
+	buildNum := context.Int("num")
+	client := circleci.NewClient(token)
+	build, resp := client.CancelBuild(context.String("user"), context.String("project"), buildNum)
+	if resp.Success() {
+		padding := 1
+		writer := tabwriter.NewWriter(os.Stdout, 0, 8, padding, '\t', tabwriter.AlignRight)
+		fmt.Fprintln(writer, "Build\tBranch\tStatus\tURL")
+		fmt.Fprintln(writer, fmt.Sprintf("%d\t%s\t%s\t%s\t", build.BuildNum, build.Branch, build.Status, build.BuildURL))
+		writer.Flush()
+		return nil
+	}
+	failed := errors.New("Failed to cancel build")
 	return failed
 }
